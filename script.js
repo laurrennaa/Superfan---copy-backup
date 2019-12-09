4f06e76f05af184a157b1ae87120d3ad425bcf8b
Lauren Larios <54549719+laurrennaa@users.noreply.github.com>
2019-12-09 09:03:56 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function resetPage() {
+	location.reload();
+}
+
+
+function moveOcto(){
+	document.getElementById("showcase").src ="images/octopus.png"
+}
+
+function movePurpFish(){
+	document.getElementById("showcase").src ="images/purplefish.png"
+}
+
+function moveTurtlee(){
+	document.getElementById("showcase").src ="images/turtle.png"
+}
+
+function moveMrshark(){
+	document.getElementById("showcase").src ="images/shark.png"
+}
+
+
+function moveStarf(){
+	document.getElementById("showcase").src ="images/starfish.png"
+}
+
+function moveOrangefish(){
+	document.getElementById("showcase").src ="images/orangefish.png"
+}
+
+function moveDolph(){
+	document.getElementById("showcase").src ="images/dolphin.png"
+}
+
+function moveWhalie(){
+	document.getElementById("showcase").src ="images/whale.png"
+}
